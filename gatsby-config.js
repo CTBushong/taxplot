c99da8272f0ef0599955a365bf2b9f28a45ccc7a
CHRIS BUSHONG <chris.bushong@gmail.com>
2020-06-13 21:10:39 -0700

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,18 @@
+const activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+console.log(`Using environment config: '${activeEnv}'`)
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: `Novela by Narative`,
-    name: `Narative`,
-    siteUrl: `https://novela.narative.co`,
+    title: `TaxPlot`,
+    name: `TaxPlot`,
+    siteUrl: `https://taxplot.com`,
     description: `This is my description that will be used in the meta tags and important for search results`,
     hero: {
-      heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
+      heading: `Welcome to TaxPlot.`,
       maxWidth: 652,
     },
     social: [
@@ -32,6 +39,24 @@ module.exports = {
     ],
   },
   plugins: [
+    
+    {
+      resolve: "gatsby-theme-firebase",
+      options: {
+        credentials: {
+          apiKey: process.env.FIREBASE_API_KEY,
+          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+          databaseURL: process.env.FIREBASE_DATABASE_URL,
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+          appId: process.env.FIREBASE_APP_ID,
+        },
+        loginPath: "/login",
+        loginRedirectPath: "/",
+        socialLogins: ["google"],
+      },
+    },
     {
       resolve: "@narative/gatsby-theme-novela",
       options: {
